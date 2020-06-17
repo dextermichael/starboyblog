029912d6965129126134e5763c15760b454edb8b
Dexter Hennington <dextermichael.h@gmail.com>
2020-06-17 16:37:58 -0400

    fix

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,8 @@
 import React from "react"
-import Wave from "../components/Wave"
+
 import "../layouts/index.css"
-import Header from "../components/Header.js"
+
 import Layout from "../layouts/index"
-import Piano from "../components/Piano"
 
 const Projects = () => (
   <div>
